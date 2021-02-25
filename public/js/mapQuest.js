456b5232958b76f9b041f496422440d2db87b893
leem3209@gmail.com <leem3209@gmail.com>
2021-02-26 02:52:47 +0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'RegUDYSANpf4H20jRMi3RiJJhqZCiM3G';
+
+	// 'map' refers to a <div> element with the ID map
+	
+	var map = L.mapquest.map('map', {
+	  center: [32.88, -117.236],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12
+	});
+	L.marker([32.88, -117.236]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+	var rsvpEmail = request.body.rsvpEmail;
+	console.log(rsvpEmail);
+
+	data.rsvp.push(revpEmail); //Add to current data
+
+	//Lets us send back some dat so that
+	//browser knows it worked!
+	response.send(rsvpEmail);
+}
